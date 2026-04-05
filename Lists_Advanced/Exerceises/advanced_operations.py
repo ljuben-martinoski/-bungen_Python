580223d0913b236288e5ba437fc87255533acafe
ljuben-martinoski <ljuben.martinoski@outlook.de>
2026-04-05 20:16:34 +0200

    05.04.2026 20:16

--- Lists_Advanced/Exerceises/advanced_operations.py
+++ Lists_Advanced/Exerceises/advanced_operations.py
@@ -2,35 +2,41 @@
 
 # a) Flatten this nested list: [[1, 2], [3, 4, 5], [6]]
 # b) Find the index of the maximum value in [23, 67, 12, 89, 45]
-# c) Create pairs from two lists: [1,2,3] and ['a','b','c'] → [(1,'a'), (2,'b'), (3,'c')]
+# c) Create pairs from two lists: [1,2,3] and ['a','b','c'] → 
+# ->[(1,'a'), (2,'b'), (3,'c')]
 # d) Remove duplicates from [1, 2, 2, 3, 4, 3, 5, 1] while preserving order
 
 
-#a)
+# a)
 nested = [[1, 2], [3, 4, 5], [6]]
-flat = [item for sublist in nested for item in sublist] # basicaly is saying that take the items from this list and put tehm in normal not nested list.
+flat = [item for sublist in nested for item in sublist]  # basicaly is saying -
+#  that take the items from this list and put tehm in normal not nested list.
 print(flat)
 
 
-#b)
+# b)
 numbers = [23, 67, 12, 89, 45]
-max_value = max(numbers) # finding the max value in the list
-max_index = numbers.index(max_value) # finding at waht index is the max value
+max_value = max(numbers)  # finding the max value in the list
+max_index = numbers.index(max_value)  # finding at waht index is the max value
 print(f"Max: {max_value} at index {max_index}")
 
 
-#c)
+# c)
 
-numb = [1,2,3]
-leter = ['a','b','c']
-combination = list(zip(numb, leter)) # list ()- funktion that converts other data types in lists, zip() funktion that takes elements from the same position in each list and pairs them together
+numb = [1, 2, 3]
+leter = ['a', 'b', 'c']
+# list ()- funktion that converts other data types in lists, zip() funktion 
+# that takes elements from the same position 
+# in each list and pairs them together
+combination = list(zip(numb, leter)) 
 print(combination)
 
-#d)
+# d)
 
 original = [1, 2, 2, 3, 4, 3, 5, 1]
 rem_duplicates = []
-[rem_duplicates.append(x) for x in original if original not in rem_duplicates] # with list comprehesions, with .append() in the empty list 
+# with list comprehesions, with .append() in the empty list 
+[rem_duplicates.append(x) for x in original if original not in rem_duplicates]  
 # then with for loop to select and remove the duplicates
 print(rem_duplicates)
 
